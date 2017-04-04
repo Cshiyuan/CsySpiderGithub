3c1795f65d87e886c6899d58a7593c6ac541c142
chenshyiuan <chenshyiuan@qq.com>
2017-04-04 21:35:43 +0800

    设置了GET添加参数的方法

--- CsySpiderGithub/spiders/github_spider.py
+++ CsySpiderGithub/spiders/github_spider.py
@@ -1,11 +1,17 @@
+#!usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@author:chenshyiuan 
+@file: github_spider.py 
+@time: 2017/04/04 
+"""
 import scrapy
 import json
 import logging
-from scrapy.selector import Selector
 from CsySpiderGithub.items import CsyspiderforgithubItem
 from CsySpiderGithub.github_repos_item import ReposItem
 
-import sys
+
 # Client ID
 # cbb90c8c9e00b757a07a
 # Client Secret
@@ -26,20 +32,10 @@ class StackSpider(scrapy.Spider):
 
     start_urls = [
         "https://api.github.com/users?since=0&client_id=cbb90c8c9e00b757a07a&client_secret=3061191bfcd5879717b3b764c574d9ec806b2aa1",
-        "https://api.github.com/users?since=30&client_id=cbb90c8c9e00b757a07a&client_secret=3061191bfcd5879717b3b764c574d9ec806b2aa1"
     ]
 
     def parse(self, response):
-        # questions = Selector(response).xpath(
-        #     '//*[@id="js-pjax-container"]/div/div[1]/div[3]')
-
-#        print('asdasdasdasdasd')
-#        print 'asdasdasd'
-#        print('asdasdasd','asdasdasd')
-#        print(sys.version_info >= (3,3))
-#        print(sys.version_info.major)
-#        print(sys.version_info.minor)
-
+        # 遍历users获取每个user的repos
         jsonresponse = json.loads(response.body)
 
         for user in jsonresponse:
@@ -53,14 +49,14 @@ class StackSpider(scrapy.Spider):
             if url_repos:
                 # print(url_repos)
                 url_repos = url_repos + \
-                    "?client_id=cbb90c8c9e00b757a07a&client_secret=3061191bfcd5879717b3b764c574d9ec806b2aa1"
+                            "?client_id=cbb90c8c9e00b757a07a&client_secret=3061191bfcd5879717b3b764c574d9ec806b2aa1"
                 logging.info(url_repos)
                 yield scrapy.Request(url_repos, self.parse_user_repos)
 
         n = 90
         while n < 1000:
             url = "https://api.github.com/users?since=" + '%d' % n + \
-                "?client_id=cbb90c8c9e00b757a07a&client_secret=3061191bfcd5879717b3b764c574d9ec806b2aa1"
+                  "?client_id=cbb90c8c9e00b757a07a&client_secret=3061191bfcd5879717b3b764c574d9ec806b2aa1"
             logging.info(url)
             n = n + 30
             yield scrapy.Request(url, self.parse)
@@ -72,11 +68,10 @@ class StackSpider(scrapy.Spider):
 
         repos_json_response = json.loads(response.body)
         for repos in repos_json_response:
-
             repos_item = ReposItem()
             repos_item['full_name'] = repos['full_name']
             repos_item['language'] = repos['language']
 
-            logging.info('try to save to mongodb with %s and language is %s' % (repos['full_name'],repos['language']))
+            logging.info('try to save to mongodb with %s and language is %s' % (repos['full_name'], repos['language']))
 
             yield repos_item
---
+++ CsySpiderGithub/util/__init__.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@author:chenshyiuan 
+@file: __init__.py.py 
+@time: 2017/04/04 
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
